88d8d5003e71f84ff8a2fe1c7c49c0eae032624b
krapli441 <tlfqj1208@gmail.com>
2023-03-06 15:55:22 +0900

    css 걷어냄

---
+++ park.js
@@ -0,0 +1,18 @@
+const header = document.getElementById('top');
+
+// header.style.cssText = "display : flex; flex-direction : column;"
+
+
+const teamname = document.querySelector('.teamname');
+
+// teamname.style.cssText = "display : flex; align-items : center; font-size : 96px; width : 100vw; height : 15vh; border-bottom : 1px solid black; justify-content : center; font-weight : 900;"
+
+teamname.style.display = "flex";
+teamname.style.alignItems = "center";
+teamname.style.fontSize = "96px";
+teamname.style.width = "100vw";
+teamname.style.height = "15vh";
+teamname.style.borderBottom = "1px solid black";
+teamname.style.justifyContent = "center";
+teamname.style.fontWeight = "900";
+
