5a4490c254eefa6eb0cd933391f48b8016797f77
gwonyejun <kyjuni1059@gmail.com>
2023-03-06 15:57:38 +0900

    js파일 업로드

---
+++ kwon.js
@@ -0,0 +1,37 @@
+// .info_text > p {
+//   font-size: 24px;
+// }
+// .info_text {
+//   display: flex;
+//   justify-content: center;
+// }
+
+
+const info_text = document.getElementsByClassName("info_text");
+const pTag = document.querySelector(".info_text > p ");
+//부모 태그 안에 자식 태그 만들기
+// function layoutTag(qnahTag,childTag){
+//   const qntag = document.getElementsByClassName(qnahTag);
+//     const tag = document.createElement(childTag);
+//     qntag.appendChild(tag);
+// }
+console.log(pTag);
+
+pTag.style.fontSize = "24px";
+
+let infoStyle = {
+  fontSize : "20xp",
+  display: "flex",
+  justifyContent : "conter"
+}
+
+let infoStylelist = ["  display","justifyContent"];
+let info = ["flex","conter"];
+
+function ptagStyle(tagName){
+  for(let i = 0; i < info.length; i++){
+    tagName.style[infoStylelist[i]] = info[i];
+  }
+}
+
+ptagStyle(info_text);
