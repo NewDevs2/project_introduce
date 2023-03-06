36777c1ee888e06115d1be0c188bcdc49ac8f6f5
gwonyejun <kyjuni1059@gmail.com>
2023-03-06 13:06:55 +0900

    중간 정검용 커밋

---
+++ obj.js
@@ -0,0 +1,20 @@
+
+    let styleName = ["height", "width", "display", "felexDirectrion", "justifyContent", "alignItems", "backgroundColor"];
+    let textStyle = [
+     "height", "fontFamily", "fontStyle", "fontSize", "color"
+    ]
+
+    let objStyle = {
+      rootStyle: ["100vh", "100vw"],
+      header: ["20%", "100%", "flex", "column", "left", "", "red"],
+      nav: ["15%", "100%", "flex", "row", "space-around", "center", "yellow"],
+      main: ["65%", "100%", "flex", "column", "space-around", "", "pink"],
+    }
+
+    let fountObj = {
+      navDiv: ["nav > div:nth-child(1)", "nav > div:nth-child(2)", "nav > div:nth-child(3)", "nav > div:nth-child(4)", "nav > div:nth-child(5)"],
+      nameText: ["박준형", "권예준", "곽윤호", "성해경", "장루빈"],
+      headerFount: ["60%","Inter", "normal", "96px"],
+      navFount: ["60%","Inter", "normal", "48px"],
+
+    }
