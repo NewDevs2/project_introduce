5cfaed2d05fb90467c90662fb832b2c253a67c58
KwakYounHo <kwakyh951@gmail.com>
2023-03-16 16:36:11 +0900

    html에 적용시킬 js파일에 모듈 적용 성공

---
+++ kwak/index.js
@@ -0,0 +1,24 @@
+// const _y = {
+//   C : (tagName,setAttri,target,wantText)=>{
+//     const E = document.createElement(tagName);
+//     if (setAttri) {
+//       for (let key in setAttri) {
+//         E.setAttribute(key,setAttri[key]);
+//       }
+//     }
+//     if (wantText) {
+//       E.innerText = wantText;
+//     }
+//     target.appendChild(E)
+//   }
+// }
+// import _y from './modules/tagMaker.js'
+// const _c = require('./modules/tagMaker.js');
+
+// const { _y } = require("./modules/tagMaker");
+import  {_y}  from './modules/tagMaker.js'
+// const yyhh = require('./test');
+// _y.C('p',{'id':'test','class':'testClass','style':'color:aqua;font-size:100px;'},document.body,'testText');
+
+// _y.C('p',{'id':'test','class':'BGC_black','style':'color:aqua; font-size:100px;'},document.body,'Test Massage');
+
